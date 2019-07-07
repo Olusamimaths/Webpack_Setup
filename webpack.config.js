b946c9fb3ff1eb5c692441793212865c1c1c4c69
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-07-07 01:37:56 +0100

    Change to sass, compile and load sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,18 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // injects the css into html
+                    "css-loader", // loads css into js
+                    "sass-loader" // 1: compiles sass to css
+                ],
+            }
+        ]
     }
+
 }
